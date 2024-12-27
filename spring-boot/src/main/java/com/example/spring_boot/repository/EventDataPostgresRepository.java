cba4b35f4e5cbdc173074c6bf51e1e030a6340e2
Ostap Pukshyn <ostap@threadable.io>
2024-12-27 12:18:40 -0700

    before merge

--- spring-boot/src/main/java/com/example/spring_boot/repository/EventDataPostgresRepository.java
+++ spring-boot/src/main/java/com/example/spring_boot/repository/EventDataPostgresRepository.java
@@ -4,6 +4,7 @@ import com.example.spring_boot.entity.EventDataPostgres;
 import com.example.spring_boot.models.CoordinateDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.stream.Stream;
@@ -11,6 +12,7 @@ import java.util.stream.Stream;
 @Repository
 public interface  EventDataPostgresRepository extends JpaRepository<EventDataPostgres, String> {
 
+    @QueryHints(@jakarta.persistence.QueryHint(name="org.hibernate.fetchSize", value="1000"))
     @Query("select new com.example.spring_boot.models.CoordinateDTO(e.latitude,e.longitude,e.timestamp) from EventDataPostgres e where e.deviceId = ?1 and e.timestamp between ?2 and ?3")
     Stream<CoordinateDTO> findByDeviceIdAndTimestampBetween(
             long deviceId,
