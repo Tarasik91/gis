db21559dff6c2d9816bab4eda2580aeb72ebc3d9
Ostap Pukshyn <ostap@threadable.io>
2025-01-03 12:56:16 -0700

    also mongo config

--- spring-boot/src/main/java/com/example/spring_boot/config/AppConfig.java
+++ spring-boot/src/main/java/com/example/spring_boot/config/AppConfig.java
@@ -24,7 +24,7 @@ public class AppConfig {
 
         @Override
         public String getDatabaseName() {
-            return "mongo_test";//"gis";
+            return "gis";
         }
 
     /*    @Override
